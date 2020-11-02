bfe81f558e86275bc686a0934003aaaf060108c8
websterek <websterek@gmail.com>
2020-11-02 21:12:25 +0100

    new VectorExtension-s and DebugExtension-s
    VectorExtension - added Ceil, Floor, Mod, Round, RoundToClosest
    DebugExtension - reimplemented base Debug functions and added DrawBox, DrawCircle

---
+++ DebugExtension.cs
@@ -0,0 +1,268 @@
+#define DEBUG_LOG_OVERWRAP
+
+using UnityEngine;
+
+#if DEBUG_LOG_OVERWRAP
+public static class Debug
+{
+    #region Debug Standard
+    public static void Break()
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.Break();
+        }
+    }
+
+    public static void ClearDeveloperConsole()
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.ClearDeveloperConsole();
+        }
+    }
+
+    public static void DebugBreak()
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.DebugBreak();
+        }
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.DrawLine(start, end, color, duration);
+        }
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.DrawLine(start, end, color);
+        }
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 end)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.DrawLine(start, end);
+        }
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 dir, Color color, float duration, bool depthTest)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.DrawLine(start, dir, color, duration, depthTest);
+        }
+    }
+
+    public static void DrawRay(Vector3 start, Vector3 dir)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.DrawLine(start, dir);
+        }
+    }
+
+    public static void Log(object message)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.Log(message);
+        }
+    }
+
+    public static void Log(object message, Object context)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.Log(message, context);
+        }
+    }
+
+    public static void LogAssertion(object message)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogAssertion(message);
+        }
+    }
+
+    public static void LogAssertionFormat(Object context, string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogAssertionFormat(context, format, args);
+        }
+    }
+
+    public static void LogAssertionFormat(string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogAssertionFormat(format, args);
+        }
+    }
+
+    public static void LogError(object message, Object context)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogError(message, context);
+        }
+    }
+
+    public static void LogError(object message)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+    }
+
+    public static void LogErrorFormat(Object context, string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogErrorFormat(context, format, args);
+        }
+    }
+
+    public static void LogErrorFormat(string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogErrorFormat(format, args);
+        }
+    }
+
+    public static void LogException(System.Exception exception)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogException(exception);
+        }
+    }
+
+    public static void LogException(System.Exception exception, Object context)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogException(exception, context);
+        }
+    }
+
+    public static void LogFormat(string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogFormat(format, args);
+        }
+    }
+
+    public static void LogFormat(Object context, string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogFormat(context, format, args);
+        }
+    }
+
+    public static void LogFormat(LogType logType, LogOption logOptions, Object context, string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogFormat(logType, logOptions, context, format, args);
+        }
+    }
+
+    public static void LogWarning(object message, Object context)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogWarning(message, context);
+        }
+    }
+
+    public static void LogWarning(object message)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogWarning(message);
+        }
+    }
+
+    public static void LogWarningFormat(Object context, string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogWarningFormat(context, format, args);
+        }
+    }
+
+    public static void LogWarningFormat(string format, params object[] args)
+    {
+        if (IsEnable())
+        {
+            UnityEngine.Debug.LogWarningFormat(format, args);
+        }
+    }
+
+    static bool IsEnable()
+    {
+        return UnityEngine.Debug.isDebugBuild;
+    }
+    #endregion
+
+
+    #region Debug Extension
+    static public void DrawBox(Vector3 point, Vector2 size, float angle, Color color, float duration = 0.0F, bool depthTest = true)
+    {
+        if (IsEnable())
+        {
+            size = size / 2;
+
+            Vector3 LT = point + new Vector3(-size.x, size.y, 0);
+            Vector3 RT = point + new Vector3(size.x, size.y, 0);
+            Vector3 LD = point + new Vector3(-size.x, -size.y, 0);
+            Vector3 RD = point + new Vector3(size.x, -size.y, 0);
+
+            LT = LT.RotateAroundPivot(point, Vector3.back, angle);
+            RT = RT.RotateAroundPivot(point, Vector3.back, angle);
+            LD = LD.RotateAroundPivot(point, Vector3.back, angle);
+            RD = RD.RotateAroundPivot(point, Vector3.back, angle);
+
+            UnityEngine.Debug.DrawLine(LT, RT, color, duration, depthTest);
+            UnityEngine.Debug.DrawLine(RT, RD, color, duration, depthTest);
+            UnityEngine.Debug.DrawLine(RD, LD, color, duration, depthTest);
+            UnityEngine.Debug.DrawLine(LD, LT, color, duration, depthTest);
+        }
+    }
+
+    static public void DrawCircle(Vector3 point, float radius, Color color, float duration = 0.0F, int segments = 16, bool depthTest = true)
+    {
+        Vector3 radiusArm = new Vector3(point.x, point.y + radius / 2, point.z);
+        float angleStep = 360 / segments;
+
+        Vector3 pointA;
+        Vector3 pointB;
+
+        for (float angle = 0; angle <= 360f; angle += angleStep)
+        {
+            pointA = Quaternion.Euler(Vector3.back * angle) * (radiusArm - point) + point;
+            pointB = Quaternion.Euler(Vector3.back * (angle + angleStep)) * (radiusArm - point) + point;
+
+            Debug.DrawLine(pointA, pointB, color, duration, depthTest);
+        }
+
+
+    }
+    #endregion
+}
+#endif
--- VectorExtension.cs
+++ VectorExtension.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public static class VectorExtension
 {
-    #region Vector3
+    #region Vector2 RotateAroundPivot
+    public static Vector2 RotateAroundPivot(this Vector2 _vector2, Vector2 pivot, float angle)
+    {
+        return RotateAroundPivot(_vector2, pivot, Quaternion.Euler(Vector3.back * angle));
+    }
+    #endregion
+
+
+    #region Vector2Int RotateAroundPivot
+    public static Vector2 RotateAroundPivot(this Vector2Int _vector2, Vector2 pivot, float angle)
+    {
+        return RotateAroundPivot(_vector2, pivot, angle);
+    }
+    #endregion
+
+
+    #region Vector3 RotateAroundPivot
     public static Vector3 RotateAroundPivot(this Vector3 _vector3, Vector3 pivot, Quaternion angle)
     {
         return angle * (_vector3 - pivot) + pivot;
@@ -22,7 +38,7 @@ public static class VectorExtension
     #endregion
 
 
-    #region Vector3Int
+    #region Vector3Int RotateAroundPivot
     public static Vector3 RotateAroundPivot(this Vector3Int _vector3, Vector3 pivot, Quaternion angle)
     {
         return angle * (_vector3 - pivot) + pivot;
@@ -40,39 +56,245 @@ public static class VectorExtension
     #endregion
 
 
-    #region Vector2
-    public static Vector2 RotateAroundPivota(this Vector2 _vector2, Vector2 pivot, float angle)
+    #region Vector3 Round
+    public static Vector3 Ceil(this Vector3 _vector3)
     {
-        return RotateAroundPivot(_vector2, pivot, Quaternion.Euler(Vector3.back * angle));
+        float x = Mathf.Ceil(_vector3.x);
+        float y = Mathf.Ceil(_vector3.y);
+        float z = Mathf.Ceil(_vector3.z);
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 Floor(this Vector3 _vector3)
+    {
+        float x = Mathf.Floor(_vector3.x);
+        float y = Mathf.Floor(_vector3.y);
+        float z = Mathf.Floor(_vector3.z);
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 Round(this Vector3 _vector3)
+    {
+        float x = Mathf.Round(_vector3.x);
+        float y = Mathf.Round(_vector3.y);
+        float z = Mathf.Round(_vector3.z);
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 Mod(this Vector3 _vector3, Vector3 vector)
+    {
+        float x = _vector3.x % vector.x;
+        float y = _vector3.y % vector.y;
+        float z = _vector3.z % vector.z;
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 Mod(this Vector3 _vector3, float vector)
+    {
+        float x = _vector3.x % vector;
+        float y = _vector3.y % vector;
+        float z = _vector3.z % vector;
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 RoundToClosest(this Vector3 _vector3, Vector3 closest)
+    {
+        float x = Mathf.Round(_vector3.x / closest.x) * closest.x;
+        float y = Mathf.Round(_vector3.y / closest.y) * closest.y;
+        float z = Mathf.Round(_vector3.z / closest.z) * closest.z;
+        return new Vector3(x, y, z);
+    }
+
+    public static Vector3 RoundToClosest(this Vector3 _vector3, float closest)
+    {
+        float x = Mathf.Round(_vector3.x / closest) * closest;
+        float y = Mathf.Round(_vector3.y / closest) * closest;
+        float z = Mathf.Round(_vector3.z / closest) * closest;
+        return new Vector3(x, y, z);
     }
     #endregion
 
 
-    #region Vector2Int
-    public static Vector2 RotateAroundPivot(this Vector2Int _vector2, Vector2 pivot, float angle)
+    #region Vector3Int Round
+    public static Vector3Int Ceil(this Vector3Int _vector3)
     {
-        return RotateAroundPivot(_vector2, pivot, angle);
+        int x = Mathf.CeilToInt(_vector3.x);
+        int y = Mathf.CeilToInt(_vector3.y);
+        int z = Mathf.CeilToInt(_vector3.z);
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int Floor(this Vector3Int _vector3)
+    {
+        int x = Mathf.FloorToInt(_vector3.x);
+        int y = Mathf.FloorToInt(_vector3.y);
+        int z = Mathf.FloorToInt(_vector3.z);
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int Mod(this Vector3Int _vector3, Vector3Int vector)
+    {
+        int x = _vector3.x % vector.x;
+        int y = _vector3.y % vector.y;
+        int z = _vector3.z % vector.z;
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int Mod(this Vector3Int _vector3, int vector)
+    {
+        int x = _vector3.x % vector;
+        int y = _vector3.y % vector;
+        int z = _vector3.z % vector;
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int Round(this Vector3Int _vector3)
+    {
+        int x = Mathf.RoundToInt(_vector3.x);
+        int y = Mathf.RoundToInt(_vector3.y);
+        int z = Mathf.RoundToInt(_vector3.z);
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int RoundToClosest(this Vector3Int _vector3, Vector3Int closest)
+    {
+        int x = Mathf.RoundToInt(_vector3.x / closest.x) * closest.x;
+        int y = Mathf.RoundToInt(_vector3.y / closest.y) * closest.y;
+        int z = Mathf.RoundToInt(_vector3.z / closest.z) * closest.z;
+        return new Vector3Int(x, y, z);
+    }
+
+    public static Vector3Int RoundToClosest(this Vector3Int _vector3, int closest)
+    {
+        int x = Mathf.RoundToInt(_vector3.x / closest) * closest;
+        int y = Mathf.RoundToInt(_vector3.y / closest) * closest;
+        int z = Mathf.RoundToInt(_vector3.z / closest) * closest;
+        return new Vector3Int(x, y, z);
     }
     #endregion
 
 
-    #region Transform
-    public static void RotateAroundPivot(this Transform _transform, Vector3 pivot, Quaternion angle)
+    #region Vector2 Round
+    public static Vector2 Ceil(this Vector2 _vector2)
     {
-        _transform.position = _transform.position.RotateAroundPivot(pivot, angle);
+        float x = Mathf.Ceil(_vector2.x);
+        float y = Mathf.Ceil(_vector2.y);
+        return new Vector3(x, y);
     }
 
-    public static void RotateAroundPivot(this Transform _transform, Vector3 pivot, Vector3 euler)
+    public static Vector2 Floor(this Vector2 _vector2)
     {
-        _transform.position = _transform.position.RotateAroundPivot(pivot, Quaternion.Euler(euler));
+        float x = Mathf.Floor(_vector2.x);
+        float y = Mathf.Floor(_vector2.y);
+        return new Vector3(x, y);
     }
 
-    public static void RotateAroundPivot(this Transform _transform, Vector3 pivot, Vector3 axis, float angle)
+    public static Vector2 Mod(this Vector2 _vector2, Vector2 vector)
     {
-        _transform.position = _transform.position.RotateAroundPivot(pivot, axis, angle);
+        float x = _vector2.x % vector.x;
+        float y = _vector2.y % vector.y;
+        return new Vector2(x, y);
+    }
+
+    public static Vector2 Mod(this Vector2 _vector2, float vector)
+    {
+        float x = _vector2.x % vector;
+        float y = _vector2.y % vector;
+        return new Vector2(x, y);
+    }
+
+    public static Vector2 Round(this Vector2 _vector2)
+    {
+        float x = Mathf.Round(_vector2.x);
+        float y = Mathf.Round(_vector2.y);
+        return new Vector2(x, y);
+    }
+
+    public static Vector2 RoundToClosest(this Vector2 _vector2, Vector2 closest)
+    {
+        float x = Mathf.Round(_vector2.x / closest.x) * closest.x;
+        float y = Mathf.Round(_vector2.y / closest.y) * closest.y;
+        return new Vector2(x, y);
+    }
+
+    public static Vector3 RoundToClosest(this Vector2 _vector2, float closest)
+    {
+        float x = Mathf.Round(_vector2.x / closest) * closest;
+        float y = Mathf.Round(_vector2.y / closest) * closest;
+        return new Vector2(x, y);
     }
     #endregion
 
 
-    // idea borrowed from: http://answers.unity.com/answers/957394/view.htmls
+    #region Vector2 Round
+    public static Vector2Int Ceil(this Vector2Int _vector2)
+    {
+        int x = Mathf.CeilToInt(_vector2.x);
+        int y = Mathf.CeilToInt(_vector2.y);
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector2Int Floor(this Vector2Int _vector2)
+    {
+        int x = Mathf.FloorToInt(_vector2.x);
+        int y = Mathf.FloorToInt(_vector2.y);
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector2Int Mod(this Vector2Int _vector2, Vector2Int vector)
+    {
+        int x = _vector2.x % vector.x;
+        int y = _vector2.y % vector.y;
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector2Int Mod(this Vector2Int _vector2, int vector)
+    {
+        int x = _vector2.x % vector;
+        int y = _vector2.y % vector;
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector2Int Round(this Vector2Int _vector2)
+    {
+        int x = Mathf.RoundToInt(_vector2.x);
+        int y = Mathf.RoundToInt(_vector2.y);
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector2Int RoundToClosest(this Vector2Int _vector2, Vector2Int closest)
+    {
+        int x = Mathf.RoundToInt(_vector2.x / closest.x) * closest.x;
+        int y = Mathf.RoundToInt(_vector2.y / closest.y) * closest.y;
+        return new Vector2Int(x, y);
+    }
+
+    public static Vector2Int RoundToClosest(this Vector2Int _vector2, int closest)
+    {
+        int x = Mathf.RoundToInt(_vector2.x / closest) * closest;
+        int y = Mathf.RoundToInt(_vector2.y / closest) * closest;
+        return new Vector2Int(x, y);
+    }
+    #endregion
+
+
+    #region Transform RotateAroundPivot
+    // public static void RotateAroundPivot(this Transform _transform, Vector3 pivot, Quaternion angle)
+    // {
+    //     _transform.position = _transform.position.RotateAroundPivot(pivot, angle);
+    // }
+
+    // public static void RotateAroundPivot(this Transform _transform, Vector3 pivot, Vector3 euler)
+    // {
+    //     _transform.position = _transform.position.RotateAroundPivot(pivot, Quaternion.Euler(euler));
+    // }
+
+    // public static void RotateAroundPivot(this Transform _transform, Vector3 pivot, Vector3 axis, float angle)
+    // {
+    //     _transform.position = _transform.position.RotateAroundPivot(pivot, axis, angle);
+    // }
+    #endregion
+
+
+    // RotateArounfPivot idea borrowed from: http://answers.unity.com/answers/957394/view.htmls
 }
